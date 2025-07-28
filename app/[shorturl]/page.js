9f505f1e92fc239cd5e7aaa7236b39aea23d0f26
ibtesamahmed2004 <ibtesama964@gmail.com>
2025-07-28 14:09:36 +0530

    Add files via upload

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,21 +1,36 @@
-import { redirect } from "next/navigation"
-import clientPromise from "@/lib/mongodb"
- 
-export default async function Page({ params }) {
-    const shorturl  = (await params).shorturl
+import Image from "next/image";
+import localFont from "next/font/local";
+import Link from "next/link";
 
-    let client = await clientPromise;
-    let db = client.db("bitlinks")
-    const collection = db.collection("url")
 
-    const doc = await collection.findOne({shorturl: shorturl})
-    if(doc) {
-      redirect(doc.url)
-    }
+const poppins = localFont({
+  src: "./fonts/Poppins-ExtraBold.ttf",
+  variable: "--font-poppins",
+});
 
-    else {
-        redirect(`${process.env.NEXT_PUBLIC_HOST}`)
-    }
+export default function Home() {
+  return (
+    <main className="bg-purple-100">
+      <section className="grid grid-cols-2 h-[50vh]">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <p className={`text-3xl font-bold ${poppins.className}`}>
+            The best URL Shortener in the Market
+          </p>
+          <p className="px-26 text-center">
+            This is the most straightforward URL Shortener in the world. Most of the URL Shorteners will track you or ask you to login and give your details. We understand your needs and hence we have created this URL Shortener.
+          </p>
+          <div className='flex gap-3 justify-start'>
+                <Link href = "/shorten"><button className='bg-purple-400 rounded-lg shadow-lg p-3 py-1 font-bold cursor-pointer text-white'>Try Now</button></Link>
+                <Link href = "/github"><button className='bg-purple-400 rounded-lg shadow-lg p-3 py-1 font-bold cursor-pointer text-white'>GitHub</button></Link>
+            </div>
+        </div>
 
-    return <div>My Post: {url}</div>
+        <div className="flex justify-start relative">
+          <img className="mix-blend-darken" alt="an image of a vector" src={"https://github.com/CodeWithHarry/Sigma-Web-Dev-Course/blob/main/Video%20136/bitlinks/public/vector.jpg?raw=true"} width={700} height={300} />
+
+        </div>
+
+      </section>
+    </main>
+  );
 }
